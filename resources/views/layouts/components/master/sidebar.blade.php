4312d38cd5af458da7091038695c0b74a9846d16
thisisgarlynugraha <thisisgarlynugraha@gmail.com>
2024-08-23 13:45:20 +0700

    FIX : View - Sidebar

--- resources/views/layouts/components/master/sidebar.blade.php
+++ resources/views/layouts/components/master/sidebar.blade.php
@@ -13,8 +13,17 @@
             <li class="{{ Request::routeIs('dashboard') ? 'active' : '' }}"><a class="nav-link" href="{{ route('dashboard') }}"><i class="fas fa-fire"></i> <span>{{ __('Dashboard') }}</span></a></li>
 
             <li class="menu-header">{{ __('Data Management') }}</li>
-            <li class="{{ Request::routeIs('operator.index') ? 'active' : '' }}"><a class="nav-link" href="{{ route('operator.index') }}"><i class="fas fa-fire"></i> <span>{{ __('Operator') }}</span></a></li>
-            <li class="{{ Request::routeIs('patient.index') ? 'active' : '' }}"><a class="nav-link" href="{{ route('patient.index') }}"><i class="fas fa-fire"></i> <span>{{ __('Patient') }}</span></a></li>
+            @hasrole('Administrator')
+                <li class="{{ Request::routeIs('operator.index') ? 'active' : '' }}"><a class="nav-link" href="{{ route('operator.index') }}"><i class="fas fa-users"></i> <span>{{ __('Operator') }}</span></a></li>
+            @endhasrole
+            
+            @hasanyrole('Administrator|Operator')
+                <li class="{{ Request::routeIs('patient.index') ? 'active' : '' }}"><a class="nav-link" href="{{ route('patient.index') }}"><i class="fas fa-hospital"></i> <span>{{ __('Patient') }}</span></a></li>
+            @endhasanyrole
+
+            @hasrole('Patient')
+                <li class="{{ Request::routeIs('patient.tensimeter.index', Auth::user()->id) ? 'active' : '' }}"><a class="nav-link" href="{{ route('patient.tensimeter.index', Auth::user()->id) }}"><i class="fas fa-heart"></i> <span>{{ __('Tensimeter') }}</span></a></li>
+            @endhasrole
         </ul>
     </aside>
 </div>
